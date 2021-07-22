82fb5d85d4f1b34e4b824576c198c4f318e579fb
Alex Wong <alexwbtg@gmail.com>
2021-07-22 10:52:54 +0800

    Add objective widget

--- Source/UnrealDemo/UnrealDemoGameModeBase.cpp
+++ Source/UnrealDemo/UnrealDemoGameModeBase.cpp
@@ -2,4 +2,14 @@
 
 
 #include "UnrealDemoGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
 
+void AUnrealDemoGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+
+    APlayerController* player_controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+    objective_widget_ = CreateWidget<UUserWidget>(player_controller, ObjectiveWidgetClass);
+    if (objective_widget_)
+        objective_widget_->AddToViewport();
+}
--- Source/UnrealDemo/UnrealDemoGameModeBase.h
+++ Source/UnrealDemo/UnrealDemoGameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
 #include "UnrealDemoGameModeBase.generated.h"
 
 /**
@@ -13,5 +14,14 @@ UCLASS()
 class UNREALDEMO_API AUnrealDemoGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	void StartPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> ObjectiveWidgetClass;
+
+private:
+	UUserWidget* objective_widget_ = nullptr;
 	
 };
